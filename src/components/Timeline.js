84c469b48f2e77c2d0fe0f0f4e7ebe8a5564224a
Arnav Jain <jainarnav@outlook.com>
2022-12-28 20:00:57 +0530

    responsive

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,7 +1,62 @@
 import React from "react";
+import robotics from "../assets/robotics.png";
+import html from "../assets/html.jpeg";
+import code from "../assets/code.jpeg";
 
 function Timeline() {
-    return <div>Timeline</div>;
+    return (
+        <div className="tl">
+            <p>
+                👋🏼 Hey There! I am Arnav Jain, a Programmer based in Mumbai, India.
+                I specialise in creating React Apps for the web and Mobile Apps using Flutter.
+            </p>
+            <div className="tl-tp">
+                <img src={robotics} />
+                <div className="tl-tp-content">
+                    <h2>2017</h2>
+                    <h3>Robotics</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                        ullamco laboris.
+                    </p>
+                </div>
+            </div>
+            <div className="year"></div>
+            <div className="tl-tp">
+                <img src={html} />
+                <div className="tl-tp-content">
+                    <h2>2018</h2>
+                    <h3>Web Development</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do Ut enim ad minim veniam, quis nostrud exercitation 
+                        ullamco laboris eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                        ullamco laboris.
+                    </p>
+                </div>
+            </div>
+            <div className="year"></div>
+            <div className="tl-tp">
+                <img src={code} />
+                <div className="tl-tp-content">
+                    <h2>2018.5</h2>
+                    <h3>Programming</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do Ut enim ad minim veniam, quis nostrud exercitation 
+                        ullamco laboris eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                        ullamco laboris. laboris eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                        ullamco laboris.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Timeline;
